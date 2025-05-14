2cd13c1b982e2fa8f30e698405d54f251ba614a2
ShyamMatam <matamshyam01@gmail.com>
2025-05-14 11:56:01 +0530

    changed

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,7 +1,10 @@
 export default function Loading() {
   return (
     <div>
-      <h1>Loading...</h1>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-blue-500"></div>
+        <h2 className="text-xl font-semibold mt-4 text-gray-700">Loading...</h2>
+      </div>
     </div>
   );
 }   
