0563ecd71a0e83e4d7a27b16d77ca1cd3716b91a
ShyamMatam <matamshyam01@gmail.com>
2025-05-14 12:19:46 +0530

    dt

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -3,7 +3,7 @@ import Nav from "@/components/nav";
 export default function NotFound() {
   return (
     <div className="flex justify-center items-center h-screen">
-      <h1 className="text-2xl font-bold">Not Found</h1>
+      <h1 className="text-2xl font-bold">Not Found - 404</h1>
     </div>
   );
 }
