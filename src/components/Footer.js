7272f218347c174e3241537fc8a0628911870814
ShyamMatam <matamshyam01@gmail.com>
2025-10-04 21:21:09 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
 return (
-    <footer className="w-full fixed bottom-0 left-0 bg-gray-900 text-gray-300 py-4 flex justify-center items-center shadow-inner z-50">
+    <footer className="w-full fixed bottom-0 left-0 bg-gray-800 text-gray-300 py-4 flex justify-center items-center shadow-inner z-50">
         <h1 className="text-lg">Footer</h1>
     </footer>
 )
